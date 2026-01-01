78c0ed23eda7a067ab69d220c7ed45fc2686e8c9
Mounisha-tech <mounisha.v.21@gmail.com>
2026-01-01 18:34:47 +0530

    Build a directed graph with distance,time, and price as weights

--- backend.py
+++ backend.py
@@ -1,3 +1,23 @@
 import pandas as pd
+import networkx as nx
+
 df=pd.read_csv("data/flights.csv")
+print("Dataset:")
 print(df)
+
+G=nx.DiGraph()
+
+for _ ,row in df.iterrows():
+    G.add_edge(row["source"],
+               row["destination"],
+               distance=row["distance"],
+               time=row["time"],
+               price=row["price"])
+
+
+print("\nNodes in graph:")
+print(G.nodes())
+
+print("\nEdges with weights:")
+for u,v,data in G.edges(data=True):
+    print(f"{u} -> {v} | {data} ")
