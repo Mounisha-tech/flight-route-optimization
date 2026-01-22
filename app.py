71fff768500d7d4cc2318f7ab6795b5c5d84846f
Mounisha-tech <mounisha.v.21@gmail.com>
2026-01-22 23:02:36 +0530

    Improve streamlit UI for interactive route selection

--- app.py
+++ app.py
@@ -4,31 +4,45 @@ from backend import load_data,build_graph,find_best_route,draw_graph
 st.set_page_config(page_title="⌯⌲ Flight Route Optimizer",layout="centered")
 
 st.title("🛫 Flight Route Optimizer")
+st.subheader("Student-friendly domestic flight route planner")
 
-df=load_data("data/flights.csv")
+st.caption("Prices and durations are based on historical data and are for comaparision purposes only")
+
+df=load_data("data/Flight Data.xlsx")
 G=build_graph(df)
 
-Source_airports=sorted(set(df["source"]).union(set(df["destination"])))
-Destination_airports=sorted(set(df["destination"]).union(set(df["source"])))
+airports=sorted(set(df["source"].tolist()+df["destination"].tolist()))
 
-source=st.selectbox("Select Source Airport",Source_airports)
-destination=st.selectbox("Select destination airport",Destination_airports)
-
-criteria=st.selectbox("Optimization Criteria",["distance","time","price"])
+with st.container():
+    source=st.selectbox("Source City",airports)
+    destination=st.selectbox("Destination City",airports)
+    criteria=st.radio(
+        "Optimize by",
+        ["price","time","distance"],
+        horizontal=True
+    )
 
 if st.button("Find Best Route"):
     if source==destination:
-        st.warning("Source and Destination cannot be the same.")
+        st.warning("Source and destination cannot be the same.")
     else:
-        path,cost=find_best_route(G,source=source,destination=destination,criteria=criteria)
-
+        with st.spinner("Finding best route..."):
+            path,cost=find_best_route(G,source,destination,criteria)
+        
         if path:
-            st.success("Best Route Found!")
+            st.success("Best route found!")
             st.write("➡️".join(path))
-            st.write(f"**Total {criteria} :** {cost}")
+            st.write(f"**Total {criteria}:** {cost}")
 
             fig=draw_graph(G,path)
             st.pyplot(fig)
-
+        
         else:
-            st.error("No route exists between selected airports")
+            st.error("No route found between selected cities")
+
+
+
+
+
+
+
